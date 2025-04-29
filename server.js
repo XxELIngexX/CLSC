beb42624c19856201b2558f59aa23d0804a9bd99
ASUSCESAR\cesar <cesar.amaya.gomes@gmail.com>
2025-04-29 18:04:34 -0500

    no message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Middleware para procesar datos JSON y formularios
 app.use(bodyParser.json());
